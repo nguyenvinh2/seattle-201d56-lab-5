45565d47012566c66ed8668132e7a362ce6aa257
Vinh Nguyen <nguyenv2@outlook.com>
2019-03-29 14:29:07 -0700

    problem 4 dones

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -81,12 +81,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let solutionArray = new Array(2);
+  let sumValue = 0;
+  for(let i = 0; i < sumArr.length; i++) {
+    sumValue = sum(sumValue, sumArr[i])[0];
+  }
+  solutionArray[0] = sumValue;
+  solutionArray[1] = `${sumArr.toString()} was passed in as an array of numbers, and ${solutionArray[0]} is their sum.`;
+  return solutionArray;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
